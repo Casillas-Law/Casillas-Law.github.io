1c8d20edb5ed77510112b80bb8162419b75c7e1f
Joe Shakely <joeshakely@gmail.com>
2020-02-19 21:22:30 -0800

    deleted .cs files

--- examples/email.js
+++ examples/email.js
@@ -1,10 +1,18 @@
-var sendemail = require('sendemail')
-var email = sendemail.email;
+// if you store your Mandrill Key in config.env file load using env2:
+var path    = require('path'); // used to resolve relative paths
+var config  = path.resolve(__dirname+'/../config.env'); // load config file
+var env     = require('env2')(config);
+
+var sendemail = require('../lib/index.js'); // no api key
+var email     = sendemail.email;
+
+var dir = __dirname + '/../examples/templates'; // unresolved
+dir = path.resolve(dir);
+sendemail.set_template_directory(dir); // set template directory
 
 var person = {
   name : "Jenny",
-  email: "your.name+test" + Math.random() + "@gmail.com", // person.email can also accept an array of emails
-  subject:"Welcome to DWYL :)"
+  email: "dwyl.test+" + Math.random() + "@gmail.com" // your email here
 }
 
 email('welcome', person, function(error, result){
