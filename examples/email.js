4202642ea926f18bb93a3cbd892eef495b08c883
Joe Shakely <joeshakely@gmail.com>
2020-02-19 23:50:30 -0800

    added AmazonSESSample.exe

--- examples/email.js
+++ examples/email.js
@@ -1,10 +1,7 @@
 // if you store your Mandrill Key in config.env file load using env2:
 var path    = require('path'); // used to resolve relative paths
 var config  = path.resolve(__dirname+'/../config.env'); // load config file
-const env = require('env2')('./path-to-your/.env');
-
-// your app goes here
-console.log(process.env.DB_HOST); // "127.0.0.1"
+var env     = require('env2')(config);
 
 var sendemail = require('../lib/index.js'); // no api key
 var email     = sendemail.email;
