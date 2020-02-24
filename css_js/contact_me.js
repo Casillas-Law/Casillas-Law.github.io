d6eef873069967ffdb7f89d693421103a63cca08
Joe Shakely <joeshakely@gmail.com>
2020-02-23 18:10:24 -0800

    google sheets js logic

--- css_js/contact_me.js
+++ css_js/contact_me.js
@@ -2,9 +2,9 @@ $(function() {
 
   $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
     preventSubmit: true,
-    submitError: function($form, event, errors) {
-      // additional error messages or events
-    },
+    // submitError: function($form, event, errors) {
+    //   // additional error messages or events
+    // },
     submitSuccess: function($form, event) {
       // event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
@@ -41,16 +41,16 @@ $(function() {
           //clear all fields
           $('#contactForm').trigger("reset");
         },
-        error: function() {
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-          $('#success > .alert-danger').append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
+        // error: function() {
+        //   // Fail message
+        //   $('#success').html("<div class='alert alert-danger'>");
+        //   $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+        //     .append("</button>");
+        //   $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+        //   $('#success > .alert-danger').append('</div>');
+        //   //clear all fields
+        //   $('#contactForm').trigger("reset");
+        // },
         complete: function() {
           setTimeout(function() {
             $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
