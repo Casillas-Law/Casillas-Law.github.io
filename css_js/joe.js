a126fb042141291801ef5ba9d83460d01b55b740
Joe Shakely <joeshakely@gmail.com>
2020-02-25 11:09:24 -0800

    color highlighting was thrown off again

---
+++ css_js/joe.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('#showEnglish').click(function() {
+      $('#showHideEn').show();
+      $('#showHideEs').hide();
+    });
+    $('.showSingle').click(function() {
+      $('.targetDiv').hide();
+      $('#div' + $(this).attr('target')).show();
+    });
+  });
