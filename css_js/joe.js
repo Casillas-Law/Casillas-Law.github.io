3ad4a1f7e1dfe7d8442208f44d666bceb049bd8a
Joe Shakely <joeshakely@gmail.com>
2020-03-04 13:46:02 -0800

    scrolling still not working

--- css_js/joe.js
+++ css_js/joe.js
@@ -1,83 +1,82 @@
 // start with spanish
-$(document).ready(function () {
-    $('#contactEn').hide();
-    $('#toggleAllEs').show();
-    $('#contact').show();
+$(document).ready(function() {
+  $("#toggleAllEs").show();
+  $("#contact").show();
+  $("#contactEn").hide();
 });
 
 // main english and spanish buttons
-$(document).ready(function () {
-    $('#toggleClickSpanish').click(function () {
-        $('#toggleAllEn').hide();
-        $('#toggleAllEs').show();
-        $('#contactEn').hide();
-        $('#contact').show();
-    });
+$(document).ready(function() {
+  $("#toggleClickSpanish").click(function() {
+    $("#toggleAllEs").show();
+    $("#contact").show();
+    $("#toggleAllEn").hide();
+    $("#contactEn").hide();
+  });
 });
 
-$(document).ready(function () {
-    $('#toggleClickEnglish').click(function () {
-        $('#toggleAllEs').hide();
-        $('#toggleAllEn').show();
-        $('#contact').hide();
-        $('#contactEn').show();
-    });
+$(document).ready(function() {
+  $("#toggleClickEnglish").click(function() {
+    $("#contactEn").show();
+    $("#toggleAllEn").show();
+    $("#toggleAllEs").hide();
+    $("#contact").hide();
+  });
 });
 
 // show services
-$(document).ready(function () {
-    $('#clickShowServicesEn').click(function () {
-        $('#toggleAllEs').hide();
-        $('#toggleAllEn').show();
-        $('#contact').hide();
-        $('#contactEn').show();
-    });
+$(document).ready(function() {
+  $("#clickShowServicesEn").click(function() {
+    $("#toggleAllEn").show();
+    $("#contactEn").show();
+    $("#toggleAllEs").hide();
+    $("#contact").hide();
+  });
 });
 
-$(document).ready(function () {
-    $('#clickShowServicesEs').click(function () {
-        $('#toggleAllEn').hide();
-        $('#toggleAllEs').show();
-        $('#contactEn').hide();
-        $('#contact').show();
-    });
+$(document).ready(function() {
+  $("#clickShowServicesEs").click(function() {
+    $("#toggleAllEs").show();
+    $("#contact").show();
+    $("#toggleAllEn").hide();
+    $("#contactEn").hide();
+  });
 });
 
 // show about
-$(document).ready(function () {
-    $('#clickShowAboutEn').click(function () {
-        $('#toggleAllEs').hide();
-        $('#toggleAllEn').show();
-        $('#contact').hide();
-        $('#contactEn').show();
-    });
+$(document).ready(function() {
+  $("#clickShowAboutEn").click(function() {
+    $("#contactEn").show();
+    $("#toggleAllEn").show();
+    $("#toggleAllEs").hide();
+    $("#contact").hide();
+  });
 });
 
-$(document).ready(function () {
-    $('#clickShowAboutEs').click(function () {
-        $('#toggleAllEn').hide();
-        $('#toggleAllEs').show();
-        $('#contactEn').hide();
-        $('#contact').show();
-    });
+$(document).ready(function() {
+  $("#clickShowAboutEs").click(function() {
+    $("#toggleAllEs").show();
+    $("#contact").show();
+    $("#toggleAllEn").hide();
+    $("#contactEn").hide();
+  });
 });
 
 // contact shows
-$(document).ready(function () {
-    $('#clickShowContactEn').click(function () {
-        $('#toggleAllEs').hide();
-        $('#toggleAllEn').show();
-        $('#contact').hide();
-        $('#contactEn').show();
-    });
+$(document).ready(function() {
+  $("#clickShowContactEn").click(function() {
+    $("#toggleAllEn").show();
+    $("#contactEn").show();
+    $("#toggleAllEs").hide();
+    $("#contact").hide();
+  });
 });
 
-$(document).ready(function () {
-    $('#clickShowContactEs').click(function () {
-        $('#toggleAllEn').hide();
-        $('#toggleAllEs').show();
-        $('#contactEn').hide();
-        $('#contact').show();
-    });
+$(document).ready(function() {
+  $("#clickShowContactEs").click(function() {
+    $("#contact").show();
+    $("#toggleAllEs").show();
+    $("#toggleAllEn").hide();
+    $("#contactEn").hide();
+  });
 });
-
